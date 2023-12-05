772ad5e2b60d69cd4c45abf079363cf1f84e98b4
Admie <91956750+admiralkaiz@users.noreply.github.com>
2023-12-05 18:55:40 +0700

    Delete src directory

--- src/main/java/com/crudexample/contoh/service/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.crudexample.contoh.service;
-import java.util.List;
-import com.crudexample.contoh.entity.Book;
-
-public interface BookService {
-    List<Book> getAllBooks();
-    Book saveBook(Book book);
-}
